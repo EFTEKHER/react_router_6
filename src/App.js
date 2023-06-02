38954935be0759277192e5197313ef2adcea42fb
EFTEKHER <eftekherali2000@gmail.com>
2023-06-02 17:04:58 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import About from './pages/About';
+import Book from './pages/Book';
+import BookList from './pages/BookList';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import NewBook from './pages/NewBook';
+import NotFound from './pages/NotFound';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <nav>
+    <ul>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/books">Books</Link></li>
+    </ul>
+    </nav>
+
+    <Routes>
+    
+    <Route path="/" element={<Home/>}/>
+    
+    <Route path="/books" element={<BookList/>}/>
+    <Route path='books/:id' element={<Book/>}/>
+
+    <Route path='books/new' element={<NewBook/>}/>
+
+    <Route path="*" element={<NotFound/>}/>
+    </Routes>
+    </>
   );
 }
 
---
+++ src/pages/BookList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+const BookList = () => {
+  return (
+    <div>
+    <h1>Book List</h1>
+    <Link to="/books/1">Book 1</Link>
+    <br />
+    <Link to="/books/2">Book 2</Link>
+    </div>
+  )
+}
+
+export default BookList
